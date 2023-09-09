85103801b2c9215ca0d39a490e624a1f5d4898a9
1Pampu <piampianimartin@gmail.com>
2023-09-09 17:27:18 -0300

    update

--- 2023/Exercise 6.py
+++ 2023/Exercise 6.py
@@ -5,3 +5,33 @@
 # * "✂️" (scissors), "🦎" (lizard), or "🖖" (Spock).
 # * Example: Input: [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]. Result: "Player 2".
 # * You should look up information on how to play with these 5 possibilities.
+
+table = {
+    "🗿":["🦎","✂️"],
+    "📄":["🗿","🖖"],
+    "✂️":["📄","🦎"],
+    "🦎":["🖖","📄"],
+    "🖖":["✂️","🗿"]
+}
+
+def calculate(list):
+    player1 = 0
+    player2 = 0
+    for tuple in list:
+        print(tuple)
+        if tuple[0] == tuple[1]:
+            pass
+        elif tuple[1] in table[tuple[0]]:
+            player1 += 1
+        elif tuple[0] in table[tuple[1]]:
+            player2 += 1
+
+    if player1 == player2:
+        print("Tie")
+    elif player1 > player2:
+        print("Player 1")
+    elif player1 < player2:
+        print("Player 2")
+
+game = [("🗿","✂️"), ("✂️","🗿"), ("📄","✂️")]
+calculate(game)
