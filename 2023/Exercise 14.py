8e958d6b56b618824e01e385bda3a205ad497c9b
1Pampu <piampianimartin@gmail.com>
2023-09-18 21:25:24 -0300

    update

---
+++ 2023/Exercise 14.py
@@ -0,0 +1,5 @@
+# * Create a function that receives a decimal number and transforms it into Octal
+# * and Hexadecimal.
+# * - It is not allowed to use built-in functions of the programming language that
+# * perform these operations directly.
+
