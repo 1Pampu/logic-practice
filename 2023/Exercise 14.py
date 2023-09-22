2ca676d90e43e7ce2fbc43386312c6cf5ae89adb
1Pampu <piampianimartin@gmail.com>
2023-09-21 23:01:45 -0300

    update

--- 2023/Exercise 14.py
+++ 2023/Exercise 14.py
@@ -3,8 +3,27 @@
 # * - It is not allowed to use built-in functions of the programming language that
 # * perform these operations directly.
 
+hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+
+def toHex(number):
+    notation = ""
+    while True:
+        quotient = number // 16
+        remainder = number % 16
+        number = quotient
+
+        notation = str(hex[remainder]) + notation
+
+        if quotient == 0:
+            break
+
+    return notation
+
+def toOct(number):
+    pass
+
 def convert(number):
     pass
 
 n = 43
-convert(n)
+toHex(255)
