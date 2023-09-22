edb4bc47f809d5412de9eada153c246f38cf5e46
1Pampu <piampianimartin@gmail.com>
2023-09-22 01:45:57 -0300

    update

--- 2023/Exercise 14.py
+++ 2023/Exercise 14.py
@@ -4,6 +4,15 @@
 # * perform these operations directly.
 
 hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
+octal = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '10', '11',
+    '12', '13', '14', '15', '16', '17', '20', '21', '22', '23',
+    '24', '25', '26', '27', '30', '31', '32', '33', '34', '35',
+    '36', '37', '40', '41', '42', '43', '44', '45', '46', '47',
+    '50', '51', '52', '53', '54', '55', '56', '57', '60', '61',
+    '62', '63', '64', '65', '66', '67', '70', '71', '72', '73',
+    '74', '75', '76', '77'
+]
 
 def toHex(number):
     notation = ""
@@ -20,10 +29,23 @@ def toHex(number):
     return notation
 
 def toOct(number):
-    pass
+    notation = ""
+    while True:
+        quotient = number // 8
+        remainder = number % 8
+        number = quotient
+
+        notation = str(hex[remainder]) + notation
+
+        if quotient == 0:
+            break
+
+    return notation
 
 def convert(number):
-    pass
+    hexValue = toHex(number)
+    octValue = toOct(number)
+    print(f'The number {number} is {hexValue} in Hex and {octValue} in Octal!')
 
-n = 43
-toHex(255)
+
+convert(255)
