891c3ce81c912500098e49973cd3610b2f41c77b
1Pampu <piampianimartin@gmail.com>
2023-09-23 20:20:23 -0300

    update

---
+++ 2023/Exercise 15.py
@@ -0,0 +1,5 @@
+# * Create a function capable of transforming Spanish into the basic language of the Star Wars universe: "Aurebesh."
+# * - You can leave characters that do not exist in "Aurebesh" untranslated.
+# * - It should also be capable of translating in the opposite direction.
+# * Did you succeed? Mention me on twitter.com/mouredev and write something to me in Aurebesh.
+# * May the Force be with you!
